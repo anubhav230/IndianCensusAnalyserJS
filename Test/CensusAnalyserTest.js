77273386c162db5809f56b0a5dc442d69c433820
anubhav singh <anubhavsingh230@gmail.com>
2020-08-24 03:43:04 +0530

    ADDED: Test case to get least populated city

--- Test/CensusAnalyserTest.js
+++ Test/CensusAnalyserTest.js
@@ -9,4 +9,9 @@ describe('indian census analyser', function(){
         const arr = csvData();
         assert.equal(arr[arr.length-1].State, 'Uttar Pradesh');
     });
+
+    it('IndianCensusAnalyser should return least populated state', function(){
+        const arr = csvData();
+        assert.equal(arr[0].State, 'Sikkim');
+    });
 });
